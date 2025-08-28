13ea25c2e59af571a5606cc30450a95de8036d55
SaadTK <tahmid.karim.saad@gmail.com>
2025-08-28 16:39:31 +0600

    fixed sameSite to none in authcontroller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -38,11 +38,10 @@ export const loginUser = async (req, res) => {
       { expiresIn: "7d" }
     );
 
-    res
-      .cookie("token", token, {
+    res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "Strict",
+        sameSite: "None",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       })
       .status(200)
